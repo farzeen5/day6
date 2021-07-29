3f541246f4ab0d5355bdad8240c83d58b4cb95dd
Farzeen <farzeenkhan1412@gmail.com>
2021-07-30 00:19:17 +0530

    perfect

--- day62.java
+++ day62.java
@@ -6,7 +6,24 @@ public class day62 {
 
 	public static void main(String[] args) {
 
-		System.out.println("welcome");
+
+			int i, Number, Sum = 0 ;
+			Scanner a = new Scanner(System.in);		
+			System.out.println(" Please Enter any Number: ");
+			Number = a.nextInt();
+
+			for(i = 1 ; i < Number ; i++) {
+				if(Number % i == 0)  {
+					Sum = Sum + i;
+				}
+			}
+			if (Sum == Number) {
+				System.out.format(" is a Perfect Number", Number);
+			}
+			else {
+				System.out.format(" d is NOT a Perfect Number", Number);
+			}
+		
 
 	}
 }
