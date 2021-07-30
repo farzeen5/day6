838628776f13d2018bba6654b60be581625e06cb
Farzeen <farzeenkhan1412@gmail.com>
2021-07-30 22:23:27 +0530

    fibo

--- day62.java
+++ day62.java
@@ -7,23 +7,17 @@ public class day62 {
 	public static void main(String[] args) {
 
 
-			int i, Number, Sum = 0 ;
-			Scanner a = new Scanner(System.in);		
-			System.out.println(" Please Enter any Number: ");
-			Number = a.nextInt();
+		int n = 10, firstTerm = 0, secondTerm = 1;
+	    System.out.println("Fibonacci Series till " + n + " terms:");
 
-			for(i = 1 ; i < Number ; i++) {
-				if(Number % i == 0)  {
-					Sum = Sum + i;
-				}
-			}
-			if (Sum == Number) {
-				System.out.format(" is a Perfect Number", Number);
-			}
-			else {
-				System.out.format(" d is NOT a Perfect Number", Number);
-			}
-		
+	    for (int i = 1; i <= n; ++i) {
+	      System.out.print(firstTerm + ", ");
+
+	      // compute the next term
+	      int nextTerm = firstTerm + secondTerm;
+	      firstTerm = secondTerm;
+	      secondTerm = nextTerm;
+	    }	
 
 	}
 }
