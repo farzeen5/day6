a54b815e62b2f9fdaf432cb1e9a0f888a0653628
Farzeen <farzeenkhan1412@gmail.com>
2021-08-02 12:37:43 +0530

    stopwatch

--- day62.java
+++ day62.java
@@ -2,30 +2,24 @@ package day6;
 
 import java.util.Scanner;
 
+
+
 public class day62 {
 
 	public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+System.out.println("Type 'start' to start time ");
+sc.next();
+long start = System.currentTimeMillis();
+System.out.println(start);
+System.out.println("type 'stop' to stop time");
+sc.next();
+sc.close();
+long stop = System.currentTimeMillis();
+System.out.println(stop);
+long elapsedTime=stop-start;
+System.out.println("Elapsed time is : " +elapsedTime);
 
-
-		int num=0;
-		Scanner reader = new Scanner(System.in);
-		System.out.println("enter a number : ");
-		num = reader.nextInt();
-	   
-	    boolean flag = false;
-		for (int i = 2; i <= num / 2; ++i) {
-	     
-	      if (num % i == 0) {
-	        flag = true;
-	        break;
-	      }
-	    }
-
-	    if (!flag)
-	      System.out.println(num + " is a prime number.");
-	    else
-	      System.out.println(num + " is not a prime number.");
-	  }
-
+	}
 	}
 
